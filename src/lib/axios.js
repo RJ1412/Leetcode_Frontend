a4aefe4349038ddfef1a36146833dba6b162a7df
Rahul Raj <135814536+RJ1412@users.noreply.github.com>
2025-07-09 11:53:21 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
-      : "https://leetcode-backend-2h6r.onrender.com/api/v1",
+      : "https://leetcode-backend-gbmq.onrender.com/api/v1",
   withCredentials: true,
 });
